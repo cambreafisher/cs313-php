8296350b0281610afcbea65cd8a07f4129089bcc
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-17 10:22:22 -0700

    adding ta2 files

---
+++ web/inclass.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>This is a php page.</h1>
+
+    <?php
+        for ($i = 0; $i < 10; $i++)
+        {
+            echo "<div>This is a div $1</div>";
+        }
+    ?>
+</body>
+</html>
