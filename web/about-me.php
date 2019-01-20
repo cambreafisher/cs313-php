9941c49a6697959916e3b78f1f7504ce27809a68
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-19 19:07:40 -0700

    adding inline style to pictures and text

--- web/about-me.php
+++ web/about-me.php
@@ -12,8 +12,8 @@
 <?php
     include 'header.php';
     ?>
-    <div>
-        <div>
+    <div class="pictures">
+        <div class="ceramics">
             <img src="https://i.pinimg.com/736x/9f/19/6b/9f196bf59060933e364140418a6eb1c0--inspirational-quote-about-life-quotes-about-life.jpg" alt="pottery">
             <div>
                 <p>Ever since I was in middle school I've wanted to take a ceramics class. So, since this is my last
@@ -22,9 +22,17 @@
 </div>
 </div>
     <img src="http://eagleriverengines.com/wp-content/uploads/2016/05/Small-engine-repair-work-includes-cleaning-of-the-engine-parts-and-casings-an-checking-all-components-for-wear-and-tolerances.jpg" alt="small engine">
-    <div>
+    <div class="smallEng">
         <p>Small Engines is another class I'm taking this semester. It's surprising how simple an engine is.
         This is another class that I'm really enjoying. I love seeing what the world is made of.</p>
+    </div>
+    <div class="webDev">
+        <img src="https://icwe2018.webengineering.org/assets/images/about-us.jpg" alt="web">
+        <div>
+            <p>I'm taking two web classes. I'm throughly enjoying them and enjoying learning and developing
+                my skills in web development.
+            </p>
+        </div>
     </div>
 </div>
 </body>
