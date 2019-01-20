654053fddfd22fccd1048c49456e88dd259232c9
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-19 18:28:00 -0700

    adding small engine pic

--- web/homepage.php
+++ web/homepage.php
@@ -13,6 +13,9 @@
     <?php
     include 'header.php';
     ?>
-    <img src="https://i.pinimg.com/736x/9f/19/6b/9f196bf59060933e364140418a6eb1c0--inspirational-quote-about-life-quotes-about-life.jpg" alt="first day of school">
-</body>
+    <div>
+    <img src="https://i.pinimg.com/736x/9f/19/6b/9f196bf59060933e364140418a6eb1c0--inspirational-quote-about-life-quotes-about-life.jpg" alt="pottery">
+    <img src="http://eagleriverengines.com/wp-content/uploads/2016/05/Small-engine-repair-work-includes-cleaning-of-the-engine-parts-and-casings-an-checking-all-components-for-wear-and-tolerances.jpg" alt="small engine">
+    </div>
+    </body>
 </html>
