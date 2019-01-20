d0e63135ac74e70b0de2c30386dcdb758a5fe235
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-19 21:21:17 -0700

    trying to add show/hide function to the about page

--- web/about-me.php
+++ web/about-me.php
@@ -21,18 +21,19 @@
 
 <div id="result"></div>
     <div class="pictures">
-        <div class="ceramics">
+        <div id="ceramics">
             <img src="https://i.pinimg.com/736x/9f/19/6b/9f196bf59060933e364140418a6eb1c0--inspirational-quote-about-life-quotes-about-life.jpg" alt="pottery">
                 <p>Ever since I was in middle school I've wanted to take a ceramics class. So, since this is my last
         semester I decided to take the class. I love working with my hands and see something come to life.
 </p>
 </div>
-<div class="smallEng">
+<div id="smallEng">
     <img src="http://eagleriverengines.com/wp-content/uploads/2016/05/Small-engine-repair-work-includes-cleaning-of-the-engine-parts-and-casings-an-checking-all-components-for-wear-and-tolerances.jpg" alt="small engine">
         <p>Small Engines is another class I'm taking this semester. It's surprising how simple an engine is.
         This is another class that I'm really enjoying. I love seeing what the world is made of.</p>
     </div>
-    <div class="webDev">
+    <button onclick="showDiv(webDev)">Web Development</button>
+<div id="webDev">
         <img src="https://icwe2018.webengineering.org/assets/images/about-us.jpg" alt="web">
             <p>I'm taking two web classes. I'm throughly enjoying them and enjoying learning and developing
                 my skills in web development.
--- web/dynamic.js
+++ web/dynamic.js
@@ -1,3 +1,11 @@
 function congrats () {
     document.getElementById("result").innerHTML = "That sounds amazing!";
 }
+function showDiv(divID) {
+    let x = document.getElementById(divID);
+    if (x.style.display === 'none') {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
