b5ab32f78d1cdf1a232dc65bc8f16af4ce501044
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-25 12:02:09 -0700

    trying to fix the layout

--- web/p03/items.php
+++ web/p03/items.php
@@ -18,18 +18,15 @@
     <img src="mainPage.png" alt="mainPage">
 </div>
 <div class="pranksnjokes">
-    <div class="snackbox">
+<div class="snackbox">
         <img src="skivingSnackbox.jpg" alt="SkivingSnackbox" class="pic">
         <h1>Skiving Snackboxes - $20.00</h1>
-    </div>
-    <div class="extendears">
+   
         <img src="extendears.jpg" alt="ExtendableEars" class="pic">
         <h1>Extendable Ears - $7.00</h1>
-    </div>
-    <div class="candy">
+    
         <img src="unopoocandy.jpg" alt="unopoocandy" class="pic">
         <h1>U-No-Poo Candies - $5.00</h1>
     </div>
-</div>
 </body>
 </html>
