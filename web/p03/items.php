c978f906debfcaeb5fd6f94895546e1478f7e970
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-24 19:06:54 -0700

    adding logo and main page image

--- web/p03/items.php
+++ web/p03/items.php
@@ -5,15 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" media="screen" href="item.css" />
-    <title>Document</title>
+    <title>Weasley's Wizard Wheezes</title>
 </head>
 <body>
     <?php
     session_start();
-
     ?>
     <div>
-     <img src="dungbomb.jpg" alt="dungbomb"/>
+     <img src="WWW.png" alt="logo" class="logo"/>
+</div>
+<div>
+    <img src="mainPage.png" alt="mainPage">
 </div>
 </body>
 </html>
