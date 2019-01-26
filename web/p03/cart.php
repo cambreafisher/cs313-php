1cc0a2fa70d589d4e419b78c9b5eaf6a9c8204c9
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-26 16:33:42 -0700

    trying to loop through the cart

--- web/p03/cart.php
+++ web/p03/cart.php
@@ -7,7 +7,7 @@ session_start();
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Your Cart</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
@@ -15,7 +15,7 @@ session_start();
     <?php
 
     foreach ($_SESSION['cart'] as $product) {
-       var_dump($product);
+        echo "<h1>$product['name']</h1>";
     }
     ?>
     
