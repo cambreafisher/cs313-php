f21504478e93b68256dfc3987f3f618f37dde4f6
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-26 19:44:58 -0700

    remove button redirect

--- web/p03/remove.php
+++ web/p03/remove.php
@@ -9,18 +9,20 @@ echo "<h1>removing $name</h1>";
 function findID($name) {
     $cart = $_SESSION['cart'];
     foreach ($cart as $key => $value) {
-        echo "<h1>$name "." $key</h1>";
+        //echo "<h1>$name "." $key</h1>";
         if ($name === $value["name"]) {
-            echo "<h1>found it</h1>";
+            //echo "<h1>found it</h1>";
             return $key;
         }
     }
 }
 $key = findID($name);
-echo "<h1>$key</h1>";
+//echo "<h1>$key</h1>";
 
 unset($_SESSION['cart'][$key]);
 
-var_dump($_SESSION['cart']);
+//var_dump($_SESSION['cart']);
+
+header('Location: cart.php');
 
 ?>
