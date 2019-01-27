0796212427f319497793f060d28e2f023ab6d001
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-01-26 18:14:54 -0700

    working on remove button...find the id first before using unset

--- web/p03/remove.php
+++ web/p03/remove.php
@@ -5,9 +5,20 @@ $name = $_POST['name'];
 
 echo "<h1>removing $name</h1>";
 
+
+function findID() {
+    foreach ($_SESSION['cart'] as $product) {
+        $arrayname = $product['name'];
+        if ($arrayname == $name) {
+            return $product;
+        }
+    }
+}
+$key = findID();
+echo "<h1>$key</h1>";
+
 unset($_SESSION['cart'][0]);
 
 var_dump($_SESSION['cart']);
-
 
 ?>
