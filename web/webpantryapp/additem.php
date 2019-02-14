352321bbb76db2f8c8daabf50b875d9cfb661285
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-02-13 22:58:42 -0700

    adding php and sql to add the food item to the database

--- web/webpantryapp/additem.php
+++ web/webpantryapp/additem.php
@@ -7,6 +7,7 @@ $foodname = htmlspecialchars($_POST['foodname']);
 try {
     $stmt = $db->prepare('INSERT INTO FOOD (food_name) 
                             VALUES (:foodname)');
+    $stmt->execute(array(':food_name => $foodname'));
 }
 catch (exception $e) {
     echo "Fail";
