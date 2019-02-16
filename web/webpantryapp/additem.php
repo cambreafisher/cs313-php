82f0e8d5582a63751b88886b11241af94d7d556a
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-02-16 12:53:47 -0700

    fixing add query

--- web/webpantryapp/additem.php
+++ web/webpantryapp/additem.php
@@ -5,16 +5,12 @@ $db = get_db();
 $foodname = htmlspecialchars($_POST['foodname']);
 $expires = htmlspecialchars($_POST['expires']);
 
-echo $foodname;
-echo $expires;
-
 try {
     $statement = $db->prepare('INSERT INTO FOOD (food_name, expiration_date) 
                             VALUES (:foodname, :expires)');
     $statement->bindValue('food_name', $foodname, PDO::PARAM_STR);
     $statement->bindValue(':expiration_date', $expires, PDO::PARAM_STR);
-    $result = $statement->execute();
-    echo $result;
+    $statement->execute();
 }
 
 /*
