3ae993c5a2a82c74f337f0821d128416530029d5
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-02-16 15:01:02 -0700

    shopping adding and removing function

--- web/webpantryapp/addshop.php
+++ web/webpantryapp/addshop.php
@@ -6,8 +6,7 @@ $foodname = htmlspecialchars($_POST['foodname']);
 
 echo $foodname;
 try {
-    $stmt = $db->prepare('INSERT INTO SHOPPING (food_name) 
-                            VALUES (:foodname)';
+    $stmt = $db->prepare('INSERT INTO SHOPPING (food_name) VALUES (:foodname)');
     $stmt->bindValue(':foodname', $foodname, PDO::PARAM_STR);
     $stmt->execute();
 }
