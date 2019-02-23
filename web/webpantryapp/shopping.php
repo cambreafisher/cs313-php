35bf7eae9308d389469ebedc7130a6f39a813537
Cambrea Fisher <cambrea.fisher@gmail.com>
2019-02-22 23:43:19 -0700

    styling shopping page

--- web/webpantryapp/shopping.php
+++ web/webpantryapp/shopping.php
@@ -8,6 +8,7 @@ $db = get_db();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" media="screen" href="shopping.css" />
     <title>Let's Go Shopping</title>
 </head>
 <body>
